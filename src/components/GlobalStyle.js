2de84f4983bcfac4cf26525de1ae69c2d6130095
Robin Métral <robin@metral.ch>
2019-01-23 20:45:22 +0100

    Position text in the center with styled components

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+export default GlobalStyle
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,19 @@
 import React from "react"
 import styled from "styled-components"
 
+import GlobalStyle from "./GlobalStyle.js"
+
 const Flex = styled.div`
+  height: 100vh;
+  width: 100vw;
   display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Layout = ({ children }) => (
   <Flex>
+    <GlobalStyle/>
     {children}
   </Flex>
 )
