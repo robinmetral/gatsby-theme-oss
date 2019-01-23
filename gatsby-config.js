fb7388d1a4ea327be66c6658412a1ac26a32780d
Robin Métral <robin@metral.ch>
2019-01-23 22:27:18 +0100

    Add typeface site variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Robin's site`,
+    title: "Robin's site",
+    description: "My description",
+    message: "Hey, I'm Robin",
+    typeface: "Lobster"
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
