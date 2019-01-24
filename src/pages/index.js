5848f1920627572e840b50ca017e9fbb43acb7fb
Robin Métral <robin@metral.ch>
2019-01-24 17:31:45 +0100

    Import pattern styles from JSON

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,14 @@ import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
 import "../styles/main.css"
+import Patterns from "../styles/patterns.json"
 
 const IndexPage = ({ data }) => (
   <div id="background"
     style={{
-      background: "linear-gradient(45deg, #92baac 45px, transparent 45px)64px 64px, linear-gradient(45deg, #92baac 45px, transparent 45px,transparent 91px, #e1ebbd 91px, #e1ebbd 135px, transparent 135px), linear-gradient(-45deg, #92baac 23px, transparent 23px, transparent 68px,#92baac 68px,#92baac 113px,transparent 113px,transparent 158px,#92baac 158px)",
-      backgroundColor: "#e1ebbd",
-      backgroundSize: "128px 128px",
+      background: Patterns.patterns[0].background,
+      backgroundColor: Patterns.patterns[0].backgroundColor,
+      backgroundSize: Patterns.patterns[0].backgroundSize,
     }}
   >
     <Helmet>
