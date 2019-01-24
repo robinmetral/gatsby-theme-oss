adde399f5caa4b0aa5eccb2496bd0e3572a14ddd
Robin Métral <robin@metral.ch>
2019-01-24 23:13:54 +0100

    Add a patterns page to view all available patterns

---
+++ src/pages/patterns.js
@@ -0,0 +1,42 @@
+import React from "react"
+import Helmet from "react-helmet"
+
+import "../styles/main.css"
+import Patterns from "../styles/patterns.json"
+
+const PatternsPage = () => {
+  return (
+    <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Available patterns</title>
+    </Helmet>
+    <p style={{textAlign: "center"}}>Hover over a pattern to see its name</p>
+    <div
+      style={{
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      }}
+    >
+      { Patterns.patterns.map(pattern => (
+      <div
+        key={pattern.name}
+        title={pattern.name}
+        style={{
+        height: "30rem",
+        minWidth: "30rem",
+        margin: "1rem",
+        backgroundImage: pattern.backgroundImage,
+        backgroundColor: pattern.backgroundColor,
+        backgroundSize: pattern.backgroundSize,
+        backgroundPosition: pattern.backgroundPosition,
+        }}
+      ></div>
+      ) ) }
+    </div>
+</>
+  )
+}
+
+export default PatternsPage
