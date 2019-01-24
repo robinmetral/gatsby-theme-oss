406c8ac1cd519fced1648b365b516e89165dfbd6
Robin Métral <robin@metral.ch>
2019-01-24 15:32:09 +0100

    Set background in JSX inline css

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,14 @@ import { Helmet } from "react-helmet"
 import "../styles/main.css"
 
 const IndexPage = ({ data }) => (
-  <>
+  <div
+    id="background"
+    style={{
+      backgroundColor: "#e1ebbd",
+      backgroundSize: "128px 128px",
+      background: "linear-gradient(45deg, #92baac 45px, transparent 45px)64px 64px, linear-gradient(45deg, #92baac 45px, transparent 45px,transparent 91px, #e1ebbd 91px, #e1ebbd 135px, transparent 135px), linear-gradient(-45deg, #92baac 23px, transparent 23px, transparent 68px,#92baac 68px,#92baac 113px,transparent 113px,transparent 158px,#92baac 158px)",
+    }}
+  >
     <Helmet>
       <meta charSet="utf-8" />
       <title>{ data.site.siteMetadata.title } | { data.site.siteMetadata.description }</title>
@@ -17,7 +24,7 @@ const IndexPage = ({ data }) => (
         color: data.site.siteMetadata.color,
       }}
     >{ data.site.siteMetadata.message }</h1>
-  </>
+  </div>
 )
 
 export default IndexPage
