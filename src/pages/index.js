a109c4b022d9ee29751861e2a26f63271be511c4
Robin Métral <robin@metral.ch>
2019-01-25 10:18:48 +0100

    Add bio on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import React from "react"
 import { graphql } from "gatsby"
 import { Helmet } from "react-helmet"
 
+import Bio from "../components/Bio"
+
 import "../styles/main.css"
 import Patterns from "../styles/patterns.json"
 
 const IndexPage = ({ data }) => {
 
-  const { title, description, pattern, message, color, typeface } = data.site.siteMetadata
+  const { title, bio, pattern, color, typeface } = data.site.siteMetadata
   const patternStyles = Patterns.patterns.find((p) => p.name === pattern)
   return (
     <div id="background"
@@ -20,7 +22,7 @@ const IndexPage = ({ data }) => {
     >
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{ title } &middot; { description }</title>
+        <title>{ title }</title>
         <link rel="stylesheet" href={ "https://fonts.googleapis.com/css?family=" + typeface } />
       </Helmet>
       <h1
@@ -28,7 +30,8 @@ const IndexPage = ({ data }) => {
         fontFamily: typeface,
         color: color,
         }}
-      >{ message }</h1>
+      >{ title }</h1>
+      <Bio bio={bio} />
     </div>
     )
 }
@@ -40,9 +43,8 @@ query {
   site {
     siteMetadata {
       title
-      description
+      bio
       pattern
-      message
       color
       typeface
     }
