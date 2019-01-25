806c14b8154a56e55b3c037f63ac6986d43543fc
Robin Métral <robin@metral.ch>
2019-01-25 11:02:03 +0100

    Style social icons list

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,7 +2,12 @@ import React from "react"
 
 const Social = (props) => {
   return (
-    <ul>
+    <ul
+      style={{
+      listStyleType: "none",
+      display: "flex",
+      }}
+    >
       <li><a href={ "https://github.com/" + props.github }>GitHub</a></li>
       <li><a href={ "https://twitter.com/" + props.twitter }>Twitter</a></li>
     </ul>
