cd375228980f2db5a4961cc9915583d7c406db8c
Robin Métral <robin@metral.ch>
2019-01-25 10:51:18 +0100

    Fix social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ import React from "react"
 const Social = (props) => {
   return (
     <ul>
-      <li href={ "https://github.com/" + props.github }>GitHub</li>
-      <li href={ "https://twitter.com/" + props.twitter }>Twitter</li>
+      <li><a href={ "https://github.com/" + props.github }>GitHub</a></li>
+      <li><a href={ "https://twitter.com/" + props.twitter }>Twitter</a></li>
     </ul>
-  )
+    )
 }
 
 export default Social
