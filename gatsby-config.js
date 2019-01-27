2f26f8ea6b351140009b64aff094a1f5cfd13bb0
Robin Métral <robin@metral.ch>
2019-01-27 21:53:57 +0100

    Add PWA manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,5 +10,17 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-starter-under-construction",
+        short_name: "Under Construction",
+        start_url: "/",
+        background_color: "#4c4c4c",
+        theme_color: "#4c4c4c",
+        display: "standalone",
+        icon: "src/images/icon.png",
+      },
+    },
   ],
 }
