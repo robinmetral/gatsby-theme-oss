dfe6a144991316539069f0561c09c9fda9f2c377
Robin Métral <robin@metral.ch>
2019-02-19 16:56:33 +0100

    Add fade-in transition to Title as per transition-group docs

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,10 +1,26 @@
 import React from "react"
+import Transition from "react-transition-group/Transition"
 
-const Title = (props) => {
-  return (
+const duration = 400
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+}
+
+const Title = (props) => (
+  <Transition in appear={true} timeout={duration}>
+    {(state) => (
     <div
       style={{
       padding: "1em",
+      ...defaultStyle,
+      ...transitionStyles[state],
       }}
     >
       <h1
@@ -18,7 +34,8 @@ const Title = (props) => {
         }}
       >{ props.title }</h1>
     </div>
-    )
-}
+    )}
+  </Transition>
+)
 
 export default Title
