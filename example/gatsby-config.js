bcfa2fd52050a542885ffe57db85c38c98e02853
Robin Métral <robin@metral.ch>
2019-10-26 11:52:36 +0200

    Use gatsby-plugin-page-creator and move data to demo

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    title: "Hey from the theme demo",
+    message:
+      "🚧 Host me on your domain while you build your new Gatsby site! (or keep me longer, that's fine too) 👷",
+    pattern: "Arrows",
+    color: "#4c4c4c",
+    titleFont: "Righteous",
+    messageFont: "Roboto",
+    social: [
+      "https://github.com/robinmetral/gatsby-starter-under-construction",
+      "https://twitter.com/robinmetral"
+    ]
+  },
   plugins: ["gatsby-theme-under-construction"]
 };
--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,15 +1,12 @@
+const path = require("path");
+
 module.exports = {
-  siteMetadata: {
-    title: "Hi, looking for a starter? 🔎",
-    message:
-      "🚧 Host me on your domain while you build your new Gatsby site! (or keep me longer, that's fine too) 👷",
-    pattern: "Arrows",
-    color: "#4c4c4c",
-    titleFont: "Righteous",
-    messageFont: "Roboto",
-    social: [
-      "https://github.com/robinmetral/gatsby-starter-under-construction",
-      "https://twitter.com/robinmetral"
-    ]
-  }
+  plugins: [
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: path.join(__dirname, `src`, `pages`)
+      }
+    }
+  ]
 };
