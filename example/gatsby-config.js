28a6601492531d2b3218848570a522f461e63ba3
Robin Métral <robin@metral.ch>
2019-10-26 15:57:59 +0200

    Pass demo data as theme options

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,16 +1,20 @@
 module.exports = {
-  siteMetadata: {
-    title: "Hey from the theme demo",
-    message:
-      "🚧 Host me on your domain while you build your new Gatsby site! (or keep me longer, that's fine too) 👷",
-    pattern: "Arrows",
-    color: "#4c4c4c",
-    titleFont: "Righteous",
-    messageFont: "Roboto",
-    social: [
-      "https://github.com/robinmetral/gatsby-starter-under-construction",
-      "https://twitter.com/robinmetral"
-    ]
-  },
-  plugins: ["gatsby-theme-under-construction"]
+  plugins: [
+    {
+      resolve: "gatsby-theme-under-construction",
+      options: {
+        title: "gatsby-theme-under-construction",
+        message:
+          "🚧 Host me on your domain while you build your new Gatsby site! (or keep me longer, that's fine too) 👷",
+        pattern: "Arrows",
+        color: "#4c4c4c",
+        titleFont: "Righteous",
+        messageFont: "Roboto",
+        social: [
+          "https://github.com/robinmetral/gatsby-starter-under-construction",
+          "https://twitter.com/robinmetral"
+        ]
+      }
+    }
+  ]
 };
