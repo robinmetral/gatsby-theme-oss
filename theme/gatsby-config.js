e62cf9e95fe6214731e56dc49f312f00b5a6d684
Robin Métral <robin@metral.ch>
2019-10-26 16:23:52 +0200

    Get data from theme consumer and spread into siteMetadata

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,12 +1,7 @@
-const path = require("path");
-
-module.exports = {
-  plugins: [
-    {
-      resolve: `gatsby-plugin-page-creator`,
-      options: {
-        path: path.join(__dirname, `src`, `pages`)
-      }
+module.exports = themeOptions => {
+  return {
+    siteMetadata: {
+      ...themeOptions
     }
-  ]
+  };
 };
