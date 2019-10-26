2ade33a35bed51f06bc79ac0916c66f22b7e82eb
Robin Métral <robin@metral.ch>
2019-10-26 09:24:10 +0200

    Install theme in example

---
+++ example/src/pages/index.js
@@ -0,0 +1,2 @@
+import React from "react";
+export default props => <h1>Hello, world</h1>;
--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -11,21 +11,5 @@ module.exports = {
       "https://github.com/robinmetral/gatsby-starter-under-construction",
       "https://twitter.com/robinmetral"
     ]
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "gatsby-starter-under-construction",
-        short_name: "Under Construction",
-        start_url: "/",
-        background_color: "#4c4c4c",
-        theme_color: "#4c4c4c",
-        display: "standalone",
-        icon: "src/images/icon.png"
-      }
-    },
-    "gatsby-plugin-offline"
-  ]
+  }
 };
